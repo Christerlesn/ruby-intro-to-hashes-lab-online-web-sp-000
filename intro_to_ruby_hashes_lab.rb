6504ecf65bfd6450c51e91957f403e63d580735c
Christerle Nounoute <christerlesn@gmail.com>
2020-09-21 18:00:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
 	monopoly = {}
 monopoly[:railroads] = {}
 monopoly[:railroads][:pieces] = 4
-monopoly[:railroads][:names] = {} 
+monopoly[:railroads][:names] = {}
 end
 
 def monopoly_with_fourth_tier
